aeb8dcaa37287821c150595424d77e763febe597
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2019-02-16 08:39:08 +0700

    Restructure public directory

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,35 +5,35 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="{{ asset('front/images/icons/favicon.ico') }}"/>
+	<link rel="icon" type="image/png" href="{{ asset('auth/images/icons/favicon.ico') }}"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('front/vendor/bootstrap/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/bootstrap/css/bootstrap.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('front/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('front/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('front/fonts/iconic/css/material-design-iconic-font.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/fonts/iconic/css/material-design-iconic-font.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('front/vendor/animate/animate.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/animate/animate.css') }}">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{ asset('front/vendor/css-hamburgers/hamburgers.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/css-hamburgers/hamburgers.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('front/vendor/animsition/css/animsition.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/animsition/css/animsition.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('front/vendor/select2/select2.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/select2/select2.min.css') }}">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{ asset('front/vendor/daterangepicker/daterangepicker.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/vendor/daterangepicker/daterangepicker.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('front/css/util.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('front/css/main.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/css/util.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('auth/css/main.css') }}">
 <!--===============================================================================================-->
 </head>
 <body style="background-color: #999999;">
 	
 	<div class="limiter">
 		<div class="container-login100">
-			<div class="login100-more" style="background-image: url('{{ asset('front/images/bg-login.jpg') }}');"></div>
+			<div class="login100-more" style="background-image: url('{{ asset('auth/images/bg-login.jpg') }}');"></div>
 
 			<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
             @yield('content')
@@ -42,21 +42,21 @@
 	</div>
 	
 <!--===============================================================================================-->
-	<script src="{{ asset('front/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+	<script src="{{ asset('auth/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('front/vendor/animsition/js/animsition.min.js') }}"></script>
+	<script src="{{ asset('auth/vendor/animsition/js/animsition.min.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('front/vendor/bootstrap/js/popper.js') }}"></script>
-	<script src="{{ asset('front/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('auth/vendor/bootstrap/js/popper.js') }}"></script>
+	<script src="{{ asset('auth/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('front/vendor/select2/select2.min.js') }}"></script>
+	<script src="{{ asset('auth/vendor/select2/select2.min.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('front/vendor/daterangepicker/moment.min.js') }}"></script>
-	<script src="{{ asset('front/vendor/daterangepicker/daterangepicker.js') }}"></script>
+	<script src="{{ asset('auth/vendor/daterangepicker/moment.min.js') }}"></script>
+	<script src="{{ asset('auth/vendor/daterangepicker/daterangepicker.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('front/vendor/countdowntime/countdowntime.js') }}"></script>
+	<script src="{{ asset('auth/vendor/countdowntime/countdowntime.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('front/js/main.js') }}"></script>
+	<script src="{{ asset('auth/js/main.js') }}"></script>
 
 </body>
 </html>
