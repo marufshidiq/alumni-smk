00cb710d83683dab197088fb8bc2074c012365a5
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2019-02-16 12:26:13 +0700

    Design Dashboard layout v1

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,126 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="product-sales-area mg-b-15">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <div class="sparkline9-list">
+                    <div class="sparkline9-hd">
+                        <div class="main-sparkline9-hd">
+                            <h1>Ringkasan</h1>
+                        </div>
+                    </div>
+                    <div class="sparkline9-graph">
+                        <div class="basic-login-form-ad">                            
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <div class="basic-login-inner">                                        
+                                        <p>Mohon lengkapi informasi profil anda untuk dapat mengakses fitur-fitur yang tersedia</p>
+                                        <form>
+                                            <div class="form-group-inner">
+                                                <div class="row">                                                    
+                                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                        <div class="i-checks pull-left">
+                                                            <label class="">
+                                                                <div class="iradio_square-green checked" style="position: relative;"><input type="radio" checked="" value="option2" name="a" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> 
+                                                                <i></i> Alamat email
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">                                                    
+                                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                        <div class="i-checks pull-left">
+                                                            <label class="">
+                                                                <div class="iradio_square-green" style="position: relative;"><input type="radio" checked="" value="option2" name="a" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> 
+                                                                <i></i> Kontak telefon
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">                                                    
+                                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                        <div class="i-checks pull-left">
+                                                            <label class="">
+                                                                <div class="iradio_square-green" style="position: relative;"><input type="radio" checked="" value="option2" name="a" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> 
+                                                                <i></i> Tahun masuk
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">                                                    
+                                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                        <div class="i-checks pull-left">
+                                                            <label class="">
+                                                                <div class="iradio_square-green" style="position: relative;"><input type="radio" checked="" value="option2" name="a" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> 
+                                                                <i></i> Jurusan
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">                                                    
+                                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                        <div class="i-checks pull-left">
+                                                            <label class="">
+                                                                <div class="iradio_square-green" style="position: relative;"><input type="radio" checked="" value="option2" name="a" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> 
+                                                                <i></i> Kelas
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">                                                    
+                                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                        <div class="i-checks pull-left">
+                                                            <label class="">
+                                                                <div class="iradio_square-green" style="position: relative;"><input type="radio" checked="" value="option2" name="a" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div> 
+                                                                <i></i> Media Sosial
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>                                                                                
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>            
+            </div>
+            <div class="col-lg-9 col-md-12 col-sm-12 col-xs-12">
+                <div class="product-sales-chart">
+                    <div class="portlet-title">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                <div class="caption pro-sl-hd">
+                                    <span class="caption-subject"><b>Statistik Alumni</b></span>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                <div class="actions graph-rp graph-rp-dl">
+                                    <p>Jumlah alumni terdaftar dan terverifikasi</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="list-inline cus-product-sl-rp">                        
+                        <li>
+                            <h5><i class="fa fa-circle" style="color: #933EC5;"></i>TKJ</h5>
+                        </li>
+                        <li>
+                            <h5><i class="fa fa-circle" style="color: #006DF0;"></i>MM</h5>
+                        </li>
+                        <li>
+                            <h5><i class="fa fa-circle" style="color: #65b12d;"></i>TPPPP</h5>
+                        </li>
+                    </ul>
+                    <div id="extra-area-chart" style="height: 356px;"></div>
+                </div>
+            </div>
+            <!-- col-lg-3 col-md-3 col-sm-3 col-xs-12 -->            
+        </div>
+    </div>
+</div>
+
+@endsection
--- resources/views/layouts/metis-nav.blade.php
+++ resources/views/layouts/metis-nav.blade.php
@@ -2,17 +2,5 @@
     <li>
         <a title="Dashboard" href="{{route('dashboard')}}" aria-expanded="false"><span class="educate-icon educate-home icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Dashboard</span></a>
     </li>
-    <li>
-        <a class="has-arrow" href="/main/index.html">
-                <span class="educate-icon educate-event icon-wrap"></span>
-                <span class="mini-click-non">Education</span>
-            </a>
-        <ul class="submenu-angle" aria-expanded="true">
-            <li><a title="Dashboard v.1" href="/main/index.html"><span class="mini-sub-pro">Dashboard v.1</span></a></li>
-            <li><a title="Dashboard v.2" href="/main/index-1.html"><span class="mini-sub-pro">Dashboard v.2</span></a></li>
-            <li><a title="Dashboard v.3" href="/main/index-2.html"><span class="mini-sub-pro">Dashboard v.3</span></a></li>
-            <li><a title="Analytics" href="/main/analytics.html"><span class="mini-sub-pro">Analytics</span></a></li>
-            <li><a title="Widgets" href="/main/widgets.html"><span class="mini-sub-pro">Widgets</span></a></li>
-        </ul>
-    </li>
+    
 </ul>
