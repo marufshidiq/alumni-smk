8fa270cf3fcc67fbbbf92aec0e8e491284eee81b
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2019-02-21 22:21:21 +0700

    Add tawk.to

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -694,6 +694,21 @@
     <!-- main JS
 		============================================ -->
     <script src="/main/js/main.js"></script>
+    @if(env('APP_PHASE') == 'production')
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='{{env('TAWK_TO_URL')}}';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+    @endif
     @yield('js')    
 </body>
 
