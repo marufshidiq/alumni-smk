ad287a3f73bf242a01a94720c71c7855d1faf2b4
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2019-03-06 06:52:30 +0700

    Change error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Oops, something is wrong | Sistem Informasi Alumni SMK N 1 Klaten</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Google Fonts
+		============================================ -->
+    <link href="https://fonts.googleapis.com/css?family=Play:400,700" rel="stylesheet">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/css/bootstrap.min.css">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/css/font-awesome.min.css">
+    <!-- owl.carousel CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/css/owl.carousel.css">
+    <link rel="stylesheet" href="/main/css/owl.theme.css">
+    <link rel="stylesheet" href="/main/css/owl.transitions.css">
+    <!-- animate CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/css/animate.css">
+    <!-- normalize CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/css/normalize.css">
+    <!-- main CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/css/main.css">
+    <!-- morrisjs CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/css/morrisjs/morris.css">
+    <!-- mCustomScrollbar CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/css/scrollbar/jquery.mCustomScrollbar.min.css">
+    <!-- metisMenu CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/css/metisMenu/metisMenu.min.css">
+    <link rel="stylesheet" href="/main/css/metisMenu/metisMenu-vertical.css">
+    <!-- calendar CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/css/calendar/fullcalendar.min.css">
+    <link rel="stylesheet" href="/main/css/calendar/fullcalendar.print.min.css">
+    <!-- style CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/style.css">
+    <!-- responsive CSS
+		============================================ -->
+    <link rel="stylesheet" href="/main/css/responsive.css">
+    <!-- modernizr JS
+		============================================ -->
+    <script src="/main/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+	<div class="error-pagewrap">
+		<div class="error-page-int">
+			<div class="content-error">
+				<h1>Server Error <span class="counter"> 500</span></h1>
+				<p>The server encountered something unexpected that didn't allow it to complete the request. We apologize.</p>
+				<a href="/">Dashboard</a>
+				<a href="https://github.com/marufshidiq/alumni-smk/issues" target="_blank">Report Problem</a>
+			</div>
+			<div class="text-center login-footer">
+            <p>Copyright © 2019. All rights reserved.<br/> <a target="_blank" href="https://github.com/marufshidiq/alumni-smk">SIA @version('version-compact')</a> - MRFSHDQ </p>
+			</div>
+		</div>   
+    </div>
+    <!-- jquery
+		============================================ -->
+    <script src="/main/js/vendor/jquery-1.12.4.min.js"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="/main/js/bootstrap.min.js"></script>
+    <!-- wow JS
+		============================================ -->
+    <script src="/main/js/wow.min.js"></script>
+    <!-- price-slider JS
+		============================================ -->
+    <script src="/main/js/jquery-price-slider.js"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="/main/js/jquery.meanmenu.js"></script>
+    <!-- owl.carousel JS
+		============================================ -->
+    <script src="/main/js/owl.carousel.min.js"></script>
+    <!-- sticky JS
+		============================================ -->
+    <script src="/main/js/jquery.sticky.js"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="/main/js/jquery.scrollUp.min.js"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="/main/js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="/main/js/scrollbar/mCustomScrollbar-active.js"></script>
+    <!-- metisMenu JS
+		============================================ -->
+    <script src="/main/js/metisMenu/metisMenu.min.js"></script>
+    <script src="/main/js/metisMenu/metisMenu-active.js"></script>
+    <!-- counterup JS
+		============================================ -->
+    <script src="/main/js/counterup/jquery.counterup.min.js"></script>
+    <script src="/main/js/counterup/waypoints.min.js"></script>
+    <script src="/main/js/counterup/counterup-active.js"></script>
+    <!-- morrisjs JS
+		============================================ -->
+    <script src="/main/js/sparkline/jquery.sparkline.min.js"></script>
+    <script src="/main/js/sparkline/jquery.charts-sparkline.js"></script>
+    <!-- calendar JS
+		============================================ -->
+    <script src="/main/js/calendar/moment.min.js"></script>
+    <script src="/main/js/calendar/fullcalendar.min.js"></script>
+    <script src="/main/js/calendar/fullcalendar-active.js"></script>
+    <!-- tab JS
+		============================================ -->
+    <script src="/main/js/tab.js"></script>
+    <!-- plugins JS
+		============================================ -->
+    <script src="/main/js/plugins.js"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="/main/js/main.js"></script>
+</body>
+
+</html>
